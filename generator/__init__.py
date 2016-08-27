fcb7285296a2e1a23dfe4b62d9a43e1038c4fa72
Steven Yu <stevenyuysy@gmail.com>
2016-08-27 10:10:38 +0800

    add comment

--- generator/__init__.py
+++ generator/__init__.py
@@ -4,17 +4,63 @@
 import random
 from time import time
 
+
 def getRandomNumber(min, max):
+
+    """Product one random number between `min` and `max`.
+
+    Args:
+        min: The minimum number
+        max: The maximum number
+
+    Returns:
+        An interger. For example: 5
+    """
+
     return random.randint(min ,max)
 
 def getRandomNumbers(min, max, length):
+
+    """Product `amount` random numbers between `min` and `max`.
+
+    Args:
+        min: The minimum number
+        max: The maximum number
+        length: The amount number
+
+    Returns:
+        An array. For example: [1, 2, 1, 5, 6, 10, 21, 50, 31]
+    """
+
     result = []
     for i in range(length):
         result.append(random.randint(min, max))
     return result
 
 def getEscRandomNumbers(min, max, length):
+
+    """Product `amount` random numbers between `min` and `max`, list at esc order.
+
+    Args:
+        min: The minimum number
+        max: The maximum number
+        length: The amount number
+
+    Returns:
+        An array. For example: [1, 2, 5, 6, 10, 21, 31]
+    """
+
     return getRandomNumbers(min, max, length).sort()
 
 def noRepeat(array):
+
+    """Product `amount` random numbers between `min` and `max`, list at esc order.
+
+    Args:
+        array: An array that need to get no-repeat data.
+
+    Returns:
+        An array. For example: [1, 2, 5, 6, 10, 21, 31]
+    """
+    
     return list(set(array))
