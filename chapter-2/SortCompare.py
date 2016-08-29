49b4dd7ca9bdd1b59468a660e6fbcd18d55f342c
Steven Yu <stevenyuysy@gmail.com>
2016-08-29 10:56:03 +0800

    compare method need improved

--- chapter-2/SortCompare.py
+++ chapter-2/SortCompare.py
@@ -9,12 +9,15 @@ from Shell import shell
 
 input = getRandomNumbers(0, 100000, 10000)
 
+# TODO(Steven) compare the funciton
 def counter(function):
+    """This is a unreasonable compare function, need to be change!
+    """
     start = time()
     function(input)
     end = time()
     return end-start
 
-print 'SortType: insertion, Time: %s' % counter(insertion)
-print 'SortType: selection, Time: %s' % counter(selection)
-print 'SortType: shell, Time: %s' % counter(shell)
+# print 'SortType: insertion, Time: %s' % counter(insertion)
+# print 'SortType: selection, Time: %s' % counter(selection)
+# print 'SortType: shell, Time: %s' % counter(shell)
