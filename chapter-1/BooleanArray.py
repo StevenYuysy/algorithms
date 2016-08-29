974c76bc05d658434abb155ba48b50d21f41be1c
Steven Yu <stevenyuysy@gmail.com>
2016-08-29 20:39:13 +0800

    update BooleanArray comment

--- chapter-1/BooleanArray.py
+++ chapter-1/BooleanArray.py
@@ -1,21 +1,29 @@
-# boolean array practice
-
-def getGCD(a, b):
-    if a < b:
-        a, b = b, a
-
-    while b != 0:
-        temp = a % b
-        a = b
-        b = temp
-
-    return True if a == 1 else False
+"""
+Write a program, creating a boolean array[][] which is N x N. If i and j are
+coprime, a[i][j] is equal to True, else is equal to Fals.
+"""
 
 def createArray(N):
+
+    def getGCD(a, b):
+        if a < b:
+            a, b = b, a
+
+        while b != 0:
+            temp = a % b
+            a = b
+            b = temp
+
+        return True if a == 1 else False
+
+
     table= [ [ 0 for i in range(N) ] for j in range(N) ]
     for i in range(N):
         for j in range(N):
             table[i][j] = getGCD(i ,j)
     return table
 
-print (createArray(6))
+if __name__ == "__main__":
+
+    N = raw_input('size --> ')
+    print (createArray(int(N)))
