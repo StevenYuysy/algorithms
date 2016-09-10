2d8c9403731b9e979064739990241c035d597f7c
Steven Yu <stevenyuysy@gmail.com>
2016-09-10 12:06:52 +0800

    add SeparateChainingHashST

---
+++ chapter-3/Dictionary.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+
+    st = {}
+    minlen = int(input('> min length of the word: '))
+
+    print('> searching tale.txt...')
+    with open('../../test-data/tale.txt') as f:
+        for dataIn in f.readlines():
+            for word in dataIn.strip().split():
+                if len(word) < minlen: continue
+                if word not in st:
+                    st[word] = 1
+                else:
+                    st[word] = st[word] + 1
+
+    maxWord = ''
+    maxVal = 0
+    for word, val in st.items():
+        if val > maxVal:
+            maxVal = val
+            maxWord = word
+    print('> most frequent word: %s %s' % (maxWord, maxVal))
--- chapter-3/SeparateChainingHashST.py
+++ chapter-3/SeparateChainingHashST.py
@@ -1,9 +1,42 @@
 #! usr/bin/python
 # -*- coding: utf-8 -*-
 
+class _ListNode(object):
+    """Create Listnode
+    """
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
 class SeparateChainingHashST(object):
 
-    def __init__(self):
-        self.__SequentialSearchST = []
+    def __init__(self, N):
+        self._table = [None] * N
+        self._N = 0 # Zero node in hash map
 
-    
+    def __len__(self):
+        return self._N
+
+    def _hash(self, key):
+        return abs(hash(key)) % len(self._table)
+
+    def get(key):
+        j = self._hash(key)
+        node = self._table[j]
+        while node is not None and node.key != key:
+            node = node.next
+        if node is None:
+            # raise KeyError, 'KeyError' + repr(key)
+            return None
+        return node
+
+    def put(key):
+        try:
+            self[key]
+        except KeyError:
+            j = self._hash(key)
+            node = self._table[j]
+            self._table[j] = _ListNode(key)
+            self._table[j].next = node
+            self._n += 1
