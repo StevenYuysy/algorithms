bada8256953e258af28bf1ef5de86b65eb183a8c
Steven Yu <stevenyuysy@gmail.com>
2016-09-11 11:17:00 +0800

    add SeparateChainingHashST

--- chapter-3/SeparateChainingHashST.py
+++ chapter-3/SeparateChainingHashST.py
@@ -5,8 +5,9 @@ class _ListNode(object):
     """Create Listnode
     """
 
-    def __init__(self, key):
+    def __init__(self, key, val):
         self.key = key
+        self.val = val
         self.next = None
 
 class SeparateChainingHashST(object):
@@ -21,22 +22,65 @@ class SeparateChainingHashST(object):
     def _hash(self, key):
         return abs(hash(key)) % len(self._table)
 
-    def get(key):
+    def get(self, key):
         j = self._hash(key)
         node = self._table[j]
         while node is not None and node.key != key:
             node = node.next
         if node is None:
-            # raise KeyError, 'KeyError' + repr(key)
             return None
-        return node
+        return node.val
 
-    def put(key):
-        try:
-            self[key]
-        except KeyError:
-            j = self._hash(key)
+    def put(self, key, val):
+        j = self._hash(key)
+        node = self._table[j]
+        while node is not None and node.key != key:
+            node = node.next
+        if node is None:
             node = self._table[j]
-            self._table[j] = _ListNode(key)
+            self._table[j] = _ListNode(key, val)
             self._table[j].next = node
-            self._n += 1
+            self._N += 1
+        else:
+            node.val = val
+
+    def contains(self, key):
+        j = self._hash(key)
+        node = self._table[j]
+        while node is not None and node.key != key:
+            node = node.next
+        if node is None:
+            return False
+        return True
+
+    def keys(self):
+        arr = []
+        for i in self._table:
+            if i is not None:
+                arr.append(i.key)
+                while i.next is not None:
+                    i = i.next
+                    arr.append(i.key)
+        return arr
+
+if __name__ == "__main__":
+
+    st = SeparateChainingHashST(997)
+    minlen = int(input('> min length of the word: '))
+
+    print('> searching tale.txt...')
+    with open('../test-data/tale.txt') as f:
+        for dataIn in f.readlines():
+            for word in dataIn.strip().split():
+                if len(word) < minlen: continue
+                if not st.contains(word):
+                    st.put(word, 1)
+                else:
+                    st.put(word, st.get(word)+1)
+
+    max = ''
+    st.put(max, 0)
+    for word in st.keys():
+        if st.get(word) > st.get(max):
+            max = word
+    print('> most frequent word: %s %s' % (max, st.get(max)))
